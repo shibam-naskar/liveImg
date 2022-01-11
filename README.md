e3cb4ce3f08b01280823e01d9c4bcfbef4c064df
shibam-naskar <shibamnaskar2@gmail.com>
2022-01-11 20:37:30 +0530

    update

--- README.md
+++ README.md
@@ -2,11 +2,19 @@
 <h2>example</h2>
 
 ```javascript
-const uploadImg = require('uploadimg');
+const uploadImg = require("liveimg");
 
-var liveUrl = uploadFileApi( YOUR_GITHUB_USERNAME, GITHUB_REPO_NAME, GITHUB_PERSONAL_ACCESSTOKEN, IMAGEDATA_BASE64);
-
-console.log(liveUrl);
+var data = await uploadImg({
+  username: YOUT_GITHUB_USERNAME,
+  repo: YOUR_REPO_NAME,
+  token: GITHUB_ACCESS_TOKEN,
+  content: IMAGE_DATA_BASE64,
+});
+console.log(data);
 ```
+
+<p>You have to create a public repo in your github acount to store all images</p>
+
+
 <h2>How To Generette Github Access Token</h2>
 <img src="https://user-images.githubusercontent.com/39475600/148953390-7d07cc59-ee32-479b-93d0-b9d2a8e38115.gif"/>
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-function getRandomString(length) {
+async function getRandomString(length) {
   var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
   var result = '';
   for ( var i = 0; i < length; i++ ) {
@@ -9,10 +9,12 @@ function getRandomString(length) {
   return result;
 }
 
-function uploadImg(username,repo,token, content) {
-    var filetype = content.toString().split(',')[0].split(';')[0].split('/')[1];
-    var filerealdata = content.toString().split(',')[1];
-    var _id = getRandomString(40);
+
+async function uploadImg(dataall) {
+    var datafinal;
+    var filetype = await dataall.content.toString().split(',')[0].split(';')[0].split('/')[1];
+    var filerealdata = await dataall.content.toString().split(',')[1];
+    var _id = await getRandomString(40);
     var data = JSON.stringify({
       message: "Uploaded by liveImg by shibam",
       content: `${filerealdata}`,
@@ -20,21 +22,23 @@ function uploadImg(username,repo,token, content) {
   
     var config = {
       method: "put",
-      url: `https://api.github.com/repos/${username}/${repo}/contents/${_id}.${filetype.toLocaleLowerCase()}`,
+      url: `https://api.github.com/repos/${dataall.username}/${dataall.repo}/contents/${_id}.${filetype.toLocaleLowerCase()}`,
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${dataall.token}`,
         "Content-Type": "application/json",
       },
       data: data,
     };
   
-    axios(config)
+    await axios(config)
       .then(function (response) {
-        console.log(JSON.stringify(response.data.content.download_url));
+        datafinal = response.data.content;
       })
       .catch(function (error) {
-        console.log(error);
+        return error;
       });
+    
+      return datafinal;
   }
 
 module.exports = uploadImg;
